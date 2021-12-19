48836f6395d5d59c410621bec59439ad9e005c94
Klibik <oleglubny2015@gmail.com>
2021-12-19 23:36:11 +0200

    ChatBot {3st} stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,24 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args){
+        String bot_name = "GOSHA";
+        int birth_year = 2021;
+        System.out.println("Hello! My name is "+bot_name+".\n"+"I was created in "+birth_year+".");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, remind me your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String user_name = scanner.nextLine();
+        System.out.println("Greate Name man. I`m glade to see you "+ user_name+"!");
+
+        System.out.println("Let me guess your age.\n"+"Enter remainders of dividing your age by 3, 5 and 7");
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int age = (remainder3*70+remainder5*21+remainder7*15)%105;
+        System.out.println("Your age is "+age+", nice age ;)");
+    }
 }
