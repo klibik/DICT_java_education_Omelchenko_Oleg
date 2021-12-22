9d946a266b6703ec66d6d4a6cb2d377c1456162e
Klibik <oleglubny2015@gmail.com>
2021-12-22 12:59:32 +0200

    3 stage hangman

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,19 +1,29 @@
 package Hangman;
 import java.util.Scanner;
+import java.util.*;
 
 public class Hangman {
 
     public static void main(String[] args) {
         System.out.println("HANGMAN\n" + "The game will be available soon.");
 
-        String words = "java";
+        Random random = new Random();
+        List<String> words = new ArrayList<>();
+        words.add("windows");
+        words.add("linux");
+        words.add("android");
+        words.add("ios");
+        words.add("macOS");
+        words.add("Nvidia");
+        words.add("AMD");
+        String hiddenword = words.get(random.nextInt(words.size()));
         Scanner in = new Scanner(System.in);
         System.out.println("Guess the word: ");
         Scanner scanner = new Scanner(System.in);
 
         String user_input = scanner.nextLine();
 
-        if (words.equals(user_input)) {
+        if (hiddenword.equals(user_input)) {
             System.out.println("You survived!");
 
         } else {
