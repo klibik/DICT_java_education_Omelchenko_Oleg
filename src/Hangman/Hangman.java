3a64b1fb0846c1049f644a8882cd791fa272f3ea
Klibik <oleglubny2015@gmail.com>
2021-12-22 13:03:23 +0200

    8 stage hanman

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -2,34 +2,72 @@ package Hangman;
 import java.util.Scanner;
 import java.util.*;
 
+
+
 public class Hangman {
-
     public static void main(String[] args) {
-        System.out.println("HANGMAN\n" + "The game will be available soon.");
+        Scanner scanner = new Scanner(System.in);
+        for(int m = 1; m > m - 1; m++){
+            System.out.print("""
+                    HANGMAN\s
+                    Type "play" to play the game, "exit" to quit:>\s
+                    """);
+            String answer = scanner.nextLine();
+            if(Objects.equals(answer,"play")){
+                game();
+            }else {
+                break;
+            }
+        }
+    }
 
+    static void game() {
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
         List<String> words = new ArrayList<>();
         words.add("windows");
         words.add("linux");
-        words.add("android");
         words.add("ios");
+        words.add("android");
         words.add("macOS");
         words.add("Nvidia");
         words.add("AMD");
         String hiddenword = words.get(random.nextInt(words.size()));
-        Scanner in = new Scanner(System.in);
-        System.out.println("Guess the word: ");
-        Scanner scanner = new Scanner(System.in);
-
-        String user_input = scanner.nextLine();
-
-        if (hiddenword.equals(user_input)) {
-            System.out.println("You survived!");
-
-        } else {
-            System.out.println("You lost!:(");
+        Set<String> word = new HashSet<>();
+        List<String> letters = new ArrayList<>();
+        for (int i = 0; i < hiddenword.length(); i++){
+            word.add(String.valueOf(hiddenword.charAt(i)));
+        }
+        String replaceword;
+        System.out.println(hiddenword.replaceAll("[a-zA-Z]","-"));
+        for(int i = 8; i >= 1;){
+            System.out.print("Input a letter:> ");
+            String letter = scanner.nextLine();
+            if(letters.contains(letter)){
+                System.out.println("You've already guessed this letter.");
+            }
+            if(letter.length() > 1){
+                System.out.println("You should input a single letter.");
+            }
+            if(word.contains(letter) && !letters.contains(letter)){
+                letters.add(letter);
+                replaceword = hiddenword.replaceAll("[^"+letters+"]","-");
+                System.out.println(replaceword);}
+            if (letter.matches("[A-Z]")){
+                System.out.println("Please enter a lowercase English letter.");
+            }
+            if (!word.contains(letter) && letter.length() < 2){
+                System.out.println("That letter doesn't appear in the word");
+                i--;
+            }
+            if (letters.size() == word.size()) {
+                System.out.println("You survived!");
+                break;
+            }
+            if (i == 0){
+                System.out.println("You dead? Hah xD !");
+            }
         }
     }
-
 
 }
